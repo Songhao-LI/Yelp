8381073ad5d2fc18b1f1d484bb54278d7c12cfd1
Sohot <sl10500@nyu.edu>
2023-12-03 21:14:43 -0500

    [init]init springboot

---
+++ server/springboot/src/test/java/com/server/yelp/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.yelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
